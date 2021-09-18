3ed675525e2abdd171bb3b3a75ed0095fe02c5c9
pvrtss <pvrtss@gmail.com>
2021-09-18 16:02:18 +0300

    Фикс библиотеки

--- lab2/code/main.py
+++ lab2/code/main.py
@@ -1,6 +1,4 @@
-from lab_python_oop.rectangle import Rectangle
-from lab_python_oop.circle import Circle
-from lab_python_oop.square import Square
+from lab_python_oop import Rectangle, Circle, Square
 from rich import print
 from rich.text import Text
 
@@ -15,28 +13,27 @@ def init_r_text(r):
     return r_text
 
 def init_c_text(c):
-    r_text = Text()
-    r_text.append("🟢")
-    r_text.append(repr(c))
-    r_text.stylize("bold", 1, 5)
-    r_text.stylize("green", 6, 14)
-    r_text.highlight_regex("\d*", style="cyan")
-    return r_text
+    c_text = Text()
+    c_text.append("🟢")
+    c_text.append(repr(c))
+    c_text.stylize("bold", 1, 5)
+    c_text.stylize("green", 6, 14)
+    c_text.highlight_regex("\d*", style="cyan")
+    return c_text
 
 def init_s_text(s):
-    r_text = Text()
-    r_text.append("🟥")
-    r_text.append(repr(s))
-    r_text.stylize("bold", 1, 8)
-    r_text.stylize("red", 9, 17)
-    r_text.highlight_regex("\d*", style="cyan")
-    return r_text
+    s_text = Text()
+    s_text.append("🟥")
+    s_text.append(repr(s))
+    s_text.stylize("bold", 1, 8)
+    s_text.stylize("red", 9, 17)
+    s_text.highlight_regex("\d*", style="cyan")
+    return s_text
 
 def main():
     r = Rectangle(5, 5, "cинего")
     c = Circle(5, "зеленого")
     s = Square(5, "красного")
-
 
     print()
     print(init_r_text(r))
